53fdf71a4076e46c76a9aa34dc6572ee37c528d5
Tyler <tylerat0258@gmail.com>
2026-04-10 16:14:05 -0500

    init commit

---
+++ heavenScript.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3          # use python3 to run this script
+
+import pyautogui as pag
+import pygetwindow as gw
+import subprocess
+import time
+import os
+import sys
+from datetime import datetime
+
+# buttons and their options (default option is the first) idea is to use image rec to find dropdown menu options and keystrokes to confirm
+# aaButton options: off, 2x, 4x, 8x
+# apiButton options: directX11, directX9, OpenGL
+# qualityButton options: low, medium, high, ultra
+# tessellationButton options: disabled, moderate, normal, extreme
+
+
